70ec35e9742ac8adaa60a182f65fb99178dc8c02
Rafi-Arsyad <arsyadr3@gmail.com>
2021-12-18 23:36:47 +0700

    Pengubahan Title

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ setting('site.title') }}</title>
+    <title>BOUNCE</title>
     <link rel="shortcut icon" href="{{ asset('template') }}/image/favicon.png" type="image/x-icon">
     <!-- Bootstrap , fonts & icons  -->
     <link rel="stylesheet" href="{{ asset('template') }}/css/bootstrap.css">
